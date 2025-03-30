b5fc1e6dd9317e83ad6a0b523432033a66b6715a
Emiliano0X1 <egp2506@gmail.com>
2025-03-29 19:14:51 -0700

    Some changes I aDDED more funciton for more testing test and i want to verufu if they are correct

--- functions.js
+++ functions.js
@@ -12,5 +12,12 @@ function sumProduct(productos,precio){
     return productos * precio;
 }
 
+function multiplitProduct(productos){
+    return productos * productos;
+}
+
+function divide(producto, precio){
+    return producto / precio;
+}
 
 module.exports = checkTotal;
